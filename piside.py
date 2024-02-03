c551fafa004d26938424769eb7a99d0add1ccd3f
Victor Jafet <pl248111@ahschool.com>
2024-02-03 03:53:35 -0500

    turning possibly 19

--- piside.py
+++ piside.py
@@ -71,9 +71,9 @@ left_PID = PID(left_kP, left_kI, left_kD, setpoint=0, output_limits=(-1023, 1023
 
 
 
-right_kP = 0.5
+right_kP = 0.2
 right_kI = 0
-right_kD = 0.5
+right_kD = 0.1
 
 right_PID = PID(right_kP, right_kI, right_kD, setpoint=0, output_limits=(-1023, 1023))
 
