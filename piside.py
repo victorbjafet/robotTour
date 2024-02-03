7c3d99668933e59ef0023eaf77661cd8cacd4469
Victor Jafet <pl248111@ahschool.com>
2024-02-03 03:15:02 -0500

    turning possibly 13

--- piside.py
+++ piside.py
@@ -55,7 +55,7 @@ def motorWrite(lMotor, rMotor):
 # 20.2 cm is the circumference of the wheels
 #180 ticks is one rotation
 
-left_kP = 1
+left_kP = 0.2
 left_kI = 0
 left_kD = 1
 
@@ -65,7 +65,7 @@ left_PID = PID(left_kP, left_kI, left_kD, setpoint=0, output_limits=(-1023, 1023
 
 right_kP = 0.2
 right_kI = 0
-right_kD = 0
+right_kD = 1
 
 right_PID = PID(right_kP, right_kI, right_kD, setpoint=0, output_limits=(-1023, 1023))
 
