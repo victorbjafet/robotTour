3820e8207b72ba1d4127c7ebe4c19aee351346e0
Victor Jafet <pl248111@ahschool.com>
2024-02-03 04:27:57 -0500

    turning possibly 22

--- piside.py
+++ piside.py
@@ -34,7 +34,7 @@ def unoStream(leftEncoder, rightEncoder):
         pass
 
 
-minSpeed = 50
+minSpeed = 75
 
 def motorWrite(lMotor, rMotor):
 
@@ -67,7 +67,7 @@ def motorWrite(lMotor, rMotor):
 #180 ticks is one rotation
         
 
-speedLimit = 100
+speedLimit = 150
 
 
 
